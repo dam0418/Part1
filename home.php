91a51113b8ae8b628e4e93fa0e97c3745bf45e9f
unknown <bushigaoshisi@gmai.com>
2024-08-14 16:06:05 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset=""UTF-8">
+    <meta name=""viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo '<h1 style="text-align: center: color: blue;">Xin chào TMU</h1>';
+echo '<a href="https://www.youtube.com/">Link</a>';
+echo '<a href="https://www.youtube.com/" rel="external" target="_blank">Youtube</a>';
+echo "# Part1" >> home.php
+?>
+</body>
+</html>
